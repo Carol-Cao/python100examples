5cfbef98f0fa7eeba48999199bedfc1d35376406
carol_cao <2628926060@qq.com>
2021-03-24 14:54:52 +0800

    add file to github

--- example98.py
+++ example98.py
@@ -1,1 +1,10 @@
-""""""
+"""从键盘输入一个字符串，将小写字母全部转换成大写字母，然后输出到一个磁盘文件"test"中保存。"""
+from sys import stdout
+path=("D:\\python_100例\\")
+
+a=input("请输入小写字母")
+b=a.upper()
+c=open(path+"test.txt","w")
+c.write(b)
+c.close()
+# print(a,b)
